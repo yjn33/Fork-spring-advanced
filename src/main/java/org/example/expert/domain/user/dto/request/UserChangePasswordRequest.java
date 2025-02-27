88f7637ee82b2b4985a6a68da6590463cfd8fcbc
yjn33 <yjn0032@gmail.com>
2025-02-27 11:52:48 +0900

    (Refactor): UserService.java 리팩토링
    chnagePassword()의 if문 리팩토링

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -5,6 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+
+// 리팩토링
+import jakarta.validation.constraints.Pattern;
+
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,6 +17,10 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
+
     @NotBlank
+    @Pattern(regexp = "^(?=.*?[0-9])(?=.*[A-Z]).{8,}", message = "새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다.")
     private String newPassword;
+    // 정규식
+
 }
